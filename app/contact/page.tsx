ec13e0c17db55fd570197b173869df1c8edc7789
Dogukan Ozturk <dogukanozturk@192.168.1.4>
2024-10-27 00:53:42 +0300

    hotfix

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,14 +1,16 @@
 import Banner from "@/components/Banner";
 import ContactForm from "@/components/ContactForm";
+import { useTranslations } from "next-intl";
 import React from "react";
 
 type Props = {};
 
 export default function ContactPage({}: Props) {
+  const t = useTranslations("Contact");
   return (
     <>
       <div>
-        <Banner title="İletişim" />
+        <Banner title={t("title")} />
         <ContactForm />
       </div>
     </>
