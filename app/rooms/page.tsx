6fe0ea81dcf38f967e265be1e52a72f61cb5a856
Dogukan Ozturk <dogukanozturk@192.168.1.4>
2024-10-26 20:05:28 +0300

    added rooms page language

--- app/rooms/page.tsx
+++ app/rooms/page.tsx
@@ -3,7 +3,6 @@ import { TbArrowsMaximize, TbUsers } from "react-icons/tb";
 import { FaHandPointer } from "react-icons/fa";
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
 import { useTranslations } from "next-intl";
 
 const rooms = [
@@ -11,35 +10,30 @@ const rooms = [
     id: "jacuzziRoom",
     price: 3256,
     image: "/jakuziPhoto/jakuzi3.jpg",
-    info: "Jakuzili Suite Oda",
     maxGuests: 2,
   },
   {
     id: "familyRoom",
     price: 1985,
     image: "/ailePhoto/aileOdası.jpg",
-    info: "Aile Odası",
     maxGuests: 4,
   },
   {
     id: "standardRoom",
     price: 1760,
     image: "/standartPhoto/standart1.jpg",
-    info: "Standart Oda",
     maxGuests: 3,
   },
   {
     id: "economicalRoom",
     price: 1375,
     image: "/ekonomikPhoto/ekonomik1.jpeg",
-    info: "Ekonomik Oda",
     maxGuests: 2,
   },
   {
     id: "superiorRoom",
     price: 2000,
     image: "/superiorPhoto/superior1.jpg",
-    info: "Superior Oda",
     maxGuests: 2,
   },
 ];
@@ -73,13 +67,13 @@ const RoomDetails = () => {
               </div>
               <div className="flex flex-col mb-4">
                 <h3 className="text-2xl font-bold" style={{ color: "#434343" }}>
-                  {room.info}
+                  {t(room.id)}
                 </h3>
                 <p
                   className="text-2xl font-bold w-52"
                   style={{ color: "#434343" }}
                 >
-                  {room.price}₺ / Gecelik
+                  {t("pricePerNight", { price: room.price })}
                 </p>
                 <div
                   className="flex items-center gap-8 mb-4"
@@ -87,18 +81,18 @@ const RoomDetails = () => {
                 >
                   <div className="flex items-center gap-2">
                     <TbUsers className="text-2xl" />
-                    <span>Kapasite: {room.maxGuests}</span>
+                    <span>{t("capacity", { maxGuests: room.maxGuests })}</span>
                   </div>
                   <div className="flex items-center gap-2">
                     <TbArrowsMaximize className="text-2xl" />
-                    <span>35m²</span>
+                    <span>{t("size")}</span>
                   </div>
                 </div>
                 <Link
                   href={`/${room.id}`}
                   className="text-[#434343] bg-[#F7C873] p-2 rounded w-full text-center flex items-center justify-center gap-2"
                 >
-                  Daha fazla bilgi için tıklayın <FaHandPointer />
+                  {t("moreInfo")} <FaHandPointer />
                 </Link>
               </div>
             </div>
